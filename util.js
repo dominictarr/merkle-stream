8d796e099adb1d7cbb7a5fb74ae4579a00ca00fb
Dominic Tarr <dominic.tarr@gmail.com>
2013-09-15 23:18:36 +0100

    this works, but it creates way too many objects.
    that makes it scale badly.
    when N=65536, 3e6 objects are created
    and N=1024, 2659 objects are created
    
    objects needed should be log(N).

--- util.js
+++ util.js
@@ -1,8 +1,7 @@
-
-
 //generate an array of ordered hashes.
 var crypto = require('crypto')
 var hashes = 0
+var OBJECTS = 0
 function hash(x) {
   hashes ++
   return crypto.createHash('sha1').update(x, 'hex').digest('hex')
@@ -16,6 +15,7 @@ exports.merkleize = merkleize
 exports.tree = tree
 
 function startsWith(a, prefix) {
+  OBJECTS ++ 
   var o = {}
   if(Array.isArray(a)) {
     for(var i in a)
@@ -29,15 +29,19 @@ function startsWith(a, prefix) {
 
 function group(o, l, iter) {
   var n = {}, cur
+  OBJECTS ++ 
   function emit(k) {
     iter(k, n)
+    OBJECTS ++ 
     n = {}
   }
 
   for(var k in o) {
+    var pre
     if(!cur)
-      cur = k.substring(0, l)
-    var pre = k.substring(0, l)
+      pre = cur = k.substring(0, l)
+    else
+      pre = k.substring(0, l)
     if(cur != pre) {
       emit(cur, n)
       cur = pre
@@ -49,9 +53,11 @@ function group(o, l, iter) {
 
 function tree (a) {
   var m = maxPrefix(a)
+  console.log('MAX', m)
   var o = startsWith(a, m)
   function recurse(o, m) {
-    if(m == 0) return o
+    if(m === 0) return o
+    OBJECTS ++
     var g = {}
     group(o, --m, function (key, group) {
       g[key] = merkleize(group)
@@ -70,20 +76,33 @@ function getHash(obj) {
 
 }
 
+function oneKey(obj) {
+  var i = 0
+  for(var k in obj) {
+    f = k
+    if(i++) return false
+  }
+  return i === 1
+}
+
+function first (o) {
+  for(var k in o)
+    return k
+}
+
 function merkleize (obj) {  
   var keys = Object.keys(obj)
-  if(keys.length === 1)
-    return obj[keys[0]]
 
-  var h = keys.map(function (e) {
-    var v = obj[e]
-    if('string' === typeof v)
-      return v
-    return v.hash
-  }).join('')
-
-  return {
-    hash: hash(h), tree: obj
+//  var h = '', n = 0
+  var h = keys.map(function (k) {
+    var v = obj[k]
+//    n ++
+    return v.hash || v //('string' === typeof v ? v : v.hash)
+  })
+  //.join('')
+ 
+  return h.length == 1 ? h[0] : OBJECTS ++, {
+    hash: hash(h.join('')), tree: obj
   }
 }
 
@@ -103,79 +122,7 @@ function table (bits) {
 
 //console.log(a)
 START = Date.now()
-/*
-function each(a, iter) {
-  for(var i in a)
-    iter(a[i], i, a)
-}
 
-function tree (a, len) {
-  var cur, acc = [], result = {}, tree = [], tree2 = {}
-  function key (r, k) {
-    return 'object' === typeof r ? k.substring(0, len) : r.substring(0, len)
-  }
-
-  function value (r) {
-    return 'object' === typeof r ? r.hash : r
-  }
-
-  each(a, function (r, k) {
-    if(!cur) {
-      cur = key(r, k)
-      acc = [], tree = [], tree2 = {}
-    }
-    var pre = key(r, k)
-    if(pre === cur) {
-      acc.push(value(r))
-      tree.push(r)
-      tree2[cur] = r
-    } else {
-      var _tree = 
-          tree.length > 1
-          ? levelOne(tree, len + 1)
-          : tree
-
-      if(acc.length == 1)
-        result[cur] = acc[0] //({pre: cur, hash: acc[0]})
-      else {
-        result[cur] = ({pre: cur, hash: hash(acc.join('')), tree: _tree})
-      }
-      cur = pre
-      acc = [value(r)]; tree = [r]; tree2 = {}
-    }
-  })
-  console.log(cur, acc, tree)
-  return result
-}
-
-function wholeTree(a, depth) {
-  //console.log(tree)
-  if(depth == null)
-    depth = maxPrefix(a)
-  if(depth === 1)
-    return tree(a, 1)
-  return wholeTree(tree(a, depth), depth - 1)
-}
-
-//var result = wholeTree(a, 3)
-
-function levelOne(a, m) {
-  var t = {}//tree(a, m)
-  var o = {}
-  for(var i in a) {
-    var v = a[i].pre || a[i]
-    if(!v.substring) {
-      console.log(a[i])
-    }
-
-    t[v.substring(0, m)] = a[i]
-  }
-  if(a.length != Object.keys(t).length) {
-    throw new Error('wrong length')
-  }
-  return t
-}
-*/
 function prefix(a, b) {
   if(a == b)
     return a.length
@@ -203,13 +150,16 @@ function maxPrefix (a) {
 
 
 if(!module.parent) {
-  var a = table(16)
+  var a = table(12)
 
   START = Date.now()
-  console.log(maxPrefix(a))
+//  console.log(maxPrefix(a))
   var H = hashes
   console.log(hashes)
   var g = tree(a)
-//  console.log(JSON.stringify(, null, 2))
-  console.log(Date.now() - START, hashes - H)
+  console.log(JSON.stringify(g, null, 2))
+  console.log(Date.now() - START, hashes - H, OBJECTS)
 }
+//
+// 331290 / 65536
+//5.055084228515625
