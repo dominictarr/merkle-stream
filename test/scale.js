6815bbd69d65acaa111f900d65b287dda91c91ae
Dominic Tarr <dominic.tarr@gmail.com>
2013-09-20 20:07:54 +0100

    update to new names

--- test/scale.js
+++ test/scale.js
@@ -2,10 +2,32 @@
 var tape = require('tape')
 
 var util = require('../util')
+var tree = require('../slow')
+var Merkle = require('../')
+tree = Merkle.tree
 
-var a = util.table(17)
+function count (tree) {
+  var s = 0
+  if('string' === typeof tree)
+    return 1
+  else
+    for (var k in tree.tree) {
+      s += count(tree.tree[k])
+    }
+  return s + 1
+}
 
+console.log('i, N, t, t/N, t/(N*log(N)), _t/t, H')
+var _t =  0.0001
+for(var i = 1; i <= 20; i++) {
 
-var g = util.prefixes(a, 0)
-
+  var a = util.table(i, 'binary')
+  var start = Date.now()
+  var g = tree(a)
+  var t = Date.now() - start || 0.0001
+  var N = 1 << i
+  console.log([i, N, t, t/N, t / (N*Math.log(N)), _t/t, count(g)].join(', '))
+//  console.log(JSON.stringify(g, null, 2))
+  _t = t
+}
 //console.log(JSON.stringify(g, null, 2))
