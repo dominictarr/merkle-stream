3fb63a9680c846e9debae7414231bcec15d3fd7b
Dominic Tarr <dominic.tarr@gmail.com>
2013-09-21 14:58:17 +0100

    iterate on tests that are small N

--- test/scale.js
+++ test/scale.js
@@ -24,23 +24,29 @@ function hash (n) {
   return crypto.createHash('sha1').update(n, 'hex').digest('hex')
 }
 
-console.log('N, t, N/t')
+console.log('N, t, N/t, hash')
 
-var _t =  0.0001
+var _t =  1
 var sqrt2 = Math.pow(2, 0.25)
 var N = 1, j = 0
 var a = []
-//for(var i = 1; i <= 20; i ++) {
+
 while(N < (1 << 21)) {
-  while(a.length < N)
+  while(a.length <= N)
     a.push(hash(j++))
   var start = Date.now()
-  var g = tree(a)
+  var m = 0, g
+  while(Date.now() === start) {
+    m ++
+    g = tree(a)
+  }
   
-  var t = Date.now() - start || 0.0001
+  var t = Date.now() - start || 1
   var d = g.digest()
-  console.log([~~N, t, N/t].join(', '))
+  console.log([a.length, t/m, a.length/(t/m), d].join(', '))
   _t = t
-  N = N * sqrt2
+  var _N = N
+  while(N < _N + 1)
+    N = N * sqrt2
 }
 
