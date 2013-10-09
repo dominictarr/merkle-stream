2e59dd28fb8a5338259df0d2b53dbb03ed8036be
Dominic Tarr <dominic.tarr@gmail.com>
2013-10-09 22:53:54 +0200

    more tests

--- test/stream-set.js
+++ test/stream-set.js
@@ -11,11 +11,52 @@ tape('stream-set, small', function (t) {
   for(var i = 0; i < 4; i++)
     s.add(2 * i + 16), z.add(2 * i + 17)
 
-  console.log('S', JSON.stringify(s.tree.toJSON(), null, 2))
-  console.log('Z', JSON.stringify(z.tree.toJSON(), null, 2))
+  //console.log('S', JSON.stringify(s.tree.toJSON(), null, 2))
+  //console.log('Z', JSON.stringify(z.tree.toJSON(), null, 2))
 
     var ss = s.createStream()
     var zs = z.createStream()
+    ss.pipe(zs).pipe(ss)
+    ss.resume(); zs.resume()
+    console.log(s.tree.digest(), z.tree.digest())
+
+    t.deepEqual(s.tree.leaves(), z.tree.leaves())
+    t.equal(s.tree.digest(), z.tree.digest())
+    t.end()
+})
+
+tape('stream-set, single', function (t) {
+  var s = Set()
+  var z = Set()
+    s.add(1)
+  //  s.add(2)
+  //  s.add(3)
+
+  //console.log('S', JSON.stringify(s.tree.toJSON(), null, 2))
+  //console.log('Z', JSON.stringify(z.tree.toJSON(), null, 2))
+
+    var ss = s.createStream()
+    var zs = z.createStream()
+    ss.pipe(zs).pipe(ss)
+    ss.resume(); zs.resume()
+    console.log(s.tree.digest(), z.tree.digest())
+
+    t.deepEqual(s.tree.leaves(), z.tree.leaves())
+    t.equal(s.tree.digest(), z.tree.digest())
+    t.end()
+})
+
+
+tape('stream-set, full vs. empty', function (t) {
+  var s = Set()
+  var z = Set()
+  for(var i = 0; i < 1; i++)
+    s.add(i)
+
+    var ss = s.createStream()
+    var zs = z.createStream()
+    ss.on('data', console.log.bind(null, 'S>'))
+    zs.on('data', console.log.bind(null, 'Z>'))
     ss.pipe(zs).pipe(ss)
     ss.resume(); zs.resume()
     console.log(s.tree.digest(), z.tree.digest())
