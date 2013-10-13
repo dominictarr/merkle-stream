fe0bf5f0b3a2e79760093d860d197515ee3dd510
Dominic Tarr <dominic.tarr@gmail.com>
2013-10-13 12:39:42 +0200

    test to compare sets, but do not replicate

---
+++ test/compare.js
@@ -0,0 +1,162 @@
+
+var Set = require('../compare')
+var tape = require('tape')
+var u = require('../util')
+var through = require('through')
+
+function clone () {
+  return through(function (data) {
+    return this.queue(JSON.parse(JSON.stringify(data)))
+  })
+}
+
+//transform an array of keys into a object [key] = true
+function toKeys (a) {
+  var o = {}
+  a.forEach(function (k) {
+    o[k] = true
+  })
+  return o
+}
+
+function union (a, b) {
+  a = toKeys(a); b = toKeys(b)
+  var o = {}
+  for(var k in a) {
+    if (a[k] && b[k])
+      o[k] = true
+  }
+  return Object.keys(o).sort()
+}
+
+//get keys in a that are not in b
+function missing (a, b) {
+  a = toKeys(a.sort()); b = toKeys(b.sort())
+  var o = {}
+  for(var k in a) {
+    if (a[k] && !b[k])
+      o[k] = true
+  }
+  return Object.keys(o).sort()
+}
+
+var DEBUG = process.env.DEBUG
+
+var log = DEBUG ? console.log : function() {}
+
+function sets (o, n, m) {
+  return function (t) {
+    var s = Set()
+    var z = Set()
+    for(var i = 0; i < o; i++)
+      s.add(i), z.add(i)
+    for(var i = 0; i < n; i++)
+      s.add(2 * (i + o))
+    for(var i = 0; i < m; i++)
+      z.add(2 * (i + o) + 1)
+
+    log(s.set, z.set)
+
+    t.deepEqual(Object.keys(s.set).sort(), s.tree.leaves().sort())
+    t.deepEqual(Object.keys(z.set).sort(), z.tree.leaves().sort())
+
+    var missing_z = missing(s.tree.leaves().sort(), z.tree.leaves().sort())
+    var missing_s = missing(z.tree.leaves().sort(), s.tree.leaves().sort())
+
+    log('in s missing from z', missing_z)
+    log('in z missing from s', missing_s)
+
+    log('********************************', s.tree.leaves().length, z.tree.leaves().length)
+    log(JSON.stringify(s.tree.toJSON(), null, 2))
+    log(JSON.stringify(z.tree.toJSON(), null, 2))
+    log('********************************')
+
+    var ss = s.createStream()
+    var zs = z.createStream()
+    ss.pipe(clone()).pipe(zs).pipe(clone()).pipe(ss)
+
+    ss.on('data', log.bind(console, 'S>'))
+    zs.on('data', log.bind(console, 'Z>'))
+
+    ss.resume(); zs.resume()
+  
+    t.deepEqual(s.missing.sort(), missing_z)
+    t.deepEqual(z.missing.sort(), missing_s)
+    
+//    t.deepEqual(s.tree.leaves(), z.tree.leaves())
+//    t.equal(s.tree.digest(), z.tree.digest())
+    t.end()
+  }
+}
+             //   shared/left/right
+tape('stream-set, 10/4/4', sets(10, 4, 4))
+//tape('stream-set, 100/40/0', sets(100, 40, 0))
+//tape('stream-set, 0/40/0', sets(0, 40, 0))
+//tape('stream-set, 0/0/6', sets(0, 0, 6))
+//tape('stream-set, 0/6/6', sets(0, 6, 6))
+//tape('stream-set, 0/7/7', sets(0, 7, 7))
+//tape('stream-set, 0/7/7', sets(0, 8, 8))
+//tape('stream-set, 0/8/8', sets(0, 8, 8))
+//tape('stream-set, 0/50/50', sets(0, 50, 50))
+//tape('stream-set, 100/50/50', sets(100, 50, 50))
+
+//tape('stream-set, small', function (t) {
+//  var s = Set()
+//  var z = Set()
+//  for(var i = 0; i < 16; i++)
+//    s.add(i), z.add(i)
+//  for(var i = 0; i < 4; i++)
+//    s.add(2 * i + 16), z.add(2 * i + 17)
+//
+//    var ss = s.createStream()
+//    var zs = z.createStream()
+//    ss.pipe(zs).pipe(ss)
+//    ss.resume(); zs.resume()
+//    console.log(s.tree.digest(), z.tree.digest())
+//
+//    t.deepEqual(s.tree.leaves(), z.tree.leaves())
+//    t.equal(s.tree.digest(), z.tree.digest())
+//    t.end()
+//})
+//
+//tape('stream-set, single', function (t) {
+//  var s = Set()
+//  var z = Set()
+//    s.add(1)
+//  //  s.add(2)
+//  //  s.add(3)
+//
+//  //console.log('S', JSON.stringify(s.tree.toJSON(), null, 2))
+//  //console.log('Z', JSON.stringify(z.tree.toJSON(), null, 2))
+//
+//    var ss = s.createStream()
+//    var zs = z.createStream()
+//    ss.pipe(clone()).pipe(zs).pipe(clone()).pipe(ss)
+//    ss.resume(); zs.resume()
+//    console.log(s.tree.digest(), z.tree.digest())
+//
+//    t.deepEqual(s.tree.leaves(), z.tree.leaves())
+//    t.equal(s.tree.digest(), z.tree.digest())
+//    t.end()
+//})
+//
+//
+//tape('stream-set, full vs. empty', function (t) {
+//  var s = Set()
+//  var z = Set()
+//  for(var i = 0; i < 1; i++)
+//    s.add(i)
+//
+//    var ss = s.createStream()
+//    var zs = z.createStream()
+//    ss.on('data', console.log.bind(null, 'S>'))
+//    zs.on('data', console.log.bind(null, 'Z>'))
+//    ss.pipe(clone()).pipe(zs).pipe(clone()).pipe(ss)
+//    ss.resume(); zs.resume()
+//    console.log(s.tree.digest(), z.tree.digest())
+//
+//    t.deepEqual(s.tree.leaves(), z.tree.leaves())
+//    t.equal(s.tree.digest(), z.tree.digest())
+//    t.end()
+//})
+
